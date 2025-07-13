8e83dcb0b0014c16dce885d6e4b3917a3d0f1b89
JDobrzanski <jakubdobrzanski@icloud.com>
2025-07-13 17:13:52 +0200

    CI/CD added

--- data/data_download.py
+++ data/data_download.py
@@ -3,6 +3,9 @@ from PIL import Image
 from torchvision import datasets
 
 def save_mnist_images(data, labels, output_dir):
+    """
+    function to save MNIST images as PNG files. 
+    """
     os.makedirs(output_dir, exist_ok=True)
     for i in range(10):
         os.makedirs(os.path.join(output_dir, str(i)), exist_ok=True)
@@ -12,12 +15,12 @@ def save_mnist_images(data, labels, output_dir):
         image = Image.fromarray(image.numpy(), mode='L')
         image.save(os.path.join(output_dir, str(label), f"{idx:05d}.png"))
 
-# Pobierz dane
+# download MNIST dataset
 train_dataset = datasets.MNIST(root='./mnist_data', train=True, download=True)
 test_dataset = datasets.MNIST(root='./mnist_data', train=False, download=True)
 
-# Zapisz jako PNG
+# save images
 save_mnist_images(train_dataset.data, train_dataset.targets, './dataset/train')
 save_mnist_images(test_dataset.data, test_dataset.targets, './dataset/test')
 
-print("Zapisano obrazy MNIST jako PNG.")
+print("image saved as PNG.")
