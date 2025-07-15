e5666016901de56decf538726a9550f083224820
JDobrzanski <jakubdobrzanski@icloud.com>
2025-07-15 21:39:07 +0200

    Fixed an example test

--- layers/backend.py
+++ layers/backend.py
@@ -4,7 +4,7 @@ USE_CPU = os.getenv("USE_CPU", "0") == "1"
 
 if USE_CPU:
     import numpy as xp
-    from numpy.lib.stride_tricks import as_strided
+    from numpy.lib.stride_tricks import as_strided  # noqa: F401
 else:
     import cupy as xp
-    from cupy.lib.stride_tricks import as_strided
+    from cupy.lib.stride_tricks import as_strided  # noqa: F401
