4e65ef06afbde633ab967f1394afa2844b45013f
JDobrzanski <jakubdobrzanski@icloud.com>
2025-07-23 02:44:17 +0200

    Added rest of tests

---
+++ tests/test_relu.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from layers.backend import xp
+from layers.relu import ReLU
+
+
+def test_relu_forward():
+    relu = ReLU()
+    input_array = xp.array([-2.0, -1.0, 0.0, 1.0, 3.0])
+    expected_output = xp.array([0.0, 0.0, 0.0, 1.0, 3.0])
+
+    output = relu.forward(input_array)
+    assert xp.array_equal(output, expected_output)
+
+
+def test_relu_backward():
+    relu = ReLU()
+    input_array = xp.array([-2.0, -1.0, 0.0, 1.0, 3.0])
+    grad_output = xp.array([1.0, 1.0, 1.0, 1.0, 1.0])
+    relu.forward(input_array)
+
+    expected_grad = xp.array([0.0, 0.0, 0.0, 1.0, 1.0])
+    grad_input = relu.backward(grad_output)
+    assert xp.array_equal(grad_input, expected_grad)
